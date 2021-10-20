0c8b3bd1b73baa1ec9c2ed6b1d1acde85fa20771
wepsree <wepsree@pm.me>
2021-10-20 16:05:22 -0700

    css

--- src/components/Hambuger.tsx
+++ src/components/Hambuger.tsx
@@ -6,7 +6,7 @@ type Props = {
 const Hambuger = ({ open, onChange }: Props) => {
   const line = `h-1 w-6 rounded-full transform bg-black`; // transition duration-300
   return (
-    <button className="md:hidden space-y-1 xy h-12 w-12 group" onClick={() => onChange(!open)}>
+    <button className="md:hidden space-y-1 xy flex-col h-12 w-12 group" onClick={() => onChange(!open)}>
       <div className={`${line} ${open ? 'rotate-45 translate-y-2' : ''}`} />
       <div className={`${line} ${open ? 'opacity-0' : ''}`} />
       <div className={`${line} ${open ? '-rotate-45 -translate-y-2' : ''}`} />
